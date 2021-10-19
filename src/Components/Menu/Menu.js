2c09f68851ad7b058809e149187d02a13a365a52
Svetlana <sl1248@yandex.ru>
2021-10-19 20:39:30 +0300

    fix database.rules.json

--- src/Components/Menu/Menu.js
+++ src/Components/Menu/Menu.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
-// import dbMenu from '../DBMenu';
 import { ListItem } from './ListItem';
 import { Banner } from './Banner';
 import { useFetch } from '../Hooks/useFetch';
